913b5afad6671362bafa670866b9ea63ea416700
Exstaa <exstagithub@gmail.com>
2024-04-30 23:03:11 +0400

    main class is created.

---
+++ SpringRestMealApplicationClass.java
@@ -0,0 +1,11 @@
+package az.developia.sent6group.springrestmeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestMealApplicationClass {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestMealApplicationClass.class, args);
+	}
+}
