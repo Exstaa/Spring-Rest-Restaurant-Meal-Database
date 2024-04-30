eb2d4e5036a22b9be5cb89a293b0154ba671a42d
Exstaa <exstagithub@gmail.com>
2024-04-30 23:03:44 +0400

    repository is ready.

---
+++ repository/MealRepository.java
@@ -0,0 +1,9 @@
+package az.developia.sent6group.springrestmeal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import az.developia.sent6group.springrestmeal.model.Meal;
+
+public interface MealRepository extends JpaRepository<Meal, Integer>{
+
+}
